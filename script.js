dbcc98f3380dc9fcb6df529e52725173a74754ba
GUchechi <godswilluchendu7@gmail.com>
2022-09-05 09:34:59 +0100

    added fetch data

---
+++ script.js
@@ -0,0 +1,13 @@
+const APIURL = 'https://api.github.com/users/';
+
+getUser("GUchechi")
+
+async function getUser(username) {
+    try{
+        const {data} = await axios(APIURL + username)
+
+        console.log(data);
+    } catch (err){
+        console.log(err);
+    }
+}
